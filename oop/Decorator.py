3aa14d58e00d64a5817e1a35ddd8fb7bae8fc3c1
Aseem Jain <Aseem.Jain@rackspace.com>
2016-03-06 22:09:30 -0600

    Decorator related code - which is similar Intercepter in spring framework with annotation.

---
+++ oop/Decorator.py
@@ -0,0 +1,28 @@
+__author__ = 'asee2278'
+
+def interceptor(func):
+
+    def wrapper_func():
+        print "code before call of func"
+        func()
+        print "code after call of func"
+    return wrapper_func
+
+def wrapper_func(func):
+    print "code before call of func"
+    func()
+    print "code after call of func"
+    return wrapper_func
+
+
+@interceptor
+def main_function():
+    print "I am the main function "
+
+#main_function = interceptor(main_function)
+main_function()
+
+# main_function = interceptor(main_function)
+# main_function1 = interceptor(main_function)
+# #main_function()
+# main_function1()
