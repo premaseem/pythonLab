b37708caa9d1580560b40bef2d22f36952d9f8ca
Aseem Jain <Aseem.Jain@rackspace.com>
2017-03-01 12:26:42 -0600

    updated game to use python 3.4

--- game_tick_tack/tick_tack_main.py
+++ game_tick_tack/tick_tack_main.py
@@ -3,51 +3,51 @@ tick = [ [None,None,None],[None,None,None],[None,None,None]]
 player = 1
 
 def printBoard() :
-    print "$$$ Current status $$$$$$$$$$$$$$ "
+    print("$$$ Current status $$$$$$$$$$$$$$ ")
     for row in tick :
-        print row
-    print "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ "
+        print (row)
+    print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ ")
 
 def inputBoard(sign,row,column) :
     if row > 2 or column > 2 :
-        print " invalid input try again "
+        print (" invalid input try again ")
         return False
-    if tick[row][column] <> None :
-        print " Cell is already occupied"
+    if tick[row][column] != None :
+        print (" Cell is already occupied")
         return False
     tick[row][column] = sign
     return True
 
 def validateRow():
     for row in tick :
-        if row[0] <> None :
+        if row[0] != None :
             if row[0] == row[1] and row[0] == row[2] :
-                print "Game Over Row victory" , row
+                print ("Game Over Row victory" , row)
                 return True
     return False
 
 
 def validateDiagonal() :
-    if tick[0][0] <> None :
+    if tick[0][0] != None :
         if tick[0][0] == tick[1][1] and tick[0][0] == tick[2][2] :
-                print "Game Over Diagonal Victory"
+                print( "Game Over Diagonal Victory")
                 return True
-    if tick[0][2] <> None :
+    if tick[0][2] != None :
         if tick[0][2] == tick[1][1] and tick[0][2] == tick[2][0] :
-                print "Game Over Diagonal Victory"
+                print ("Game Over Diagonal Victory")
                 return True
     return False
 
 def validateColumn(num) :
-    if tick[0][num] <> None :
+    if tick[0][num] != None :
             if tick[0][num] == tick[1][num] and tick[0][num] == tick[2][num] :
-                print "Game Over Column victory"
+                print ("Game Over Column victory")
                 return True
     return False
 
 def validateBoard():
     if validateRow() or validateDiagonal() or validateColumn(0) or validateColumn(1) or validateColumn(2) or valiateDraw() :
-        print "Thanks for playing "
+        print ("Thanks for playing ")
         return True
 
     return False
@@ -58,18 +58,18 @@ def valiateDraw() :
             if tick[x][y] == None :
                 # print "Game is a Tie ;-) "
                 return False
-    print "Game is a Tie ;-) "
+    print ("Game is a Tie ;-) ")
     return True
 
-print "welcome to Tic Tack game ... "
+print ("welcome to Tic Tack game ... ")
 
 def takeInput(ply):
 
     if ply == 1 : s = 'X'
     else : s = '0'
-    print "player {} with sign {} take your turn and select coordinate".format(ply,s)
-    r = int(raw_input("Enter Row ... "))
-    c = int(raw_input("Enter Column ... "))
+    print ("player {} with sign {} take your turn and select coordinate".format(ply,s))
+    r = int(input("Enter Row ... "))
+    c = int(input("Enter Column ... "))
 
     return inputBoard(s,r,c)
 
@@ -86,7 +86,7 @@ while not validateBoard() :
         counter = counter + 1
     printBoard()
 
-print "Congratulation for winning :-) "
+print ("Congratulation for winning :-) ")
 
 
 
