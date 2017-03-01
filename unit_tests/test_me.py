2cd52307311b1bc47b29b574ddc586f2dea33113
Aseem Jain <Aseem.Jain@rackspace.com>
2017-03-01 14:55:20 -0600

    Added test fixtures. Fixtures are reusable code which gets executes with test hooks. which means before test is run and after completion of test.

--- unit_tests/test_me.py
+++ unit_tests/test_me.py
@@ -8,6 +8,19 @@ def raises_error(*args, **kwds):
 
 class OutcomesTest(unittest.TestCase):
 
+    def setUp(self):
+        print('In setUp()')
+        self.fixture = range(1, 10)
+
+    def tearDown(self):
+        print('In tearDown()')
+        del self.fixture
+
+    def test(self):
+        print('in test()')
+        self.assertEqual(self.fixture, range(1, 10))
+
+
     def test_pass(self):
         self.assertTrue(True)
 
@@ -24,6 +37,7 @@ class OutcomesTest(unittest.TestCase):
 
     def test_assert_raises(self):
         self.assertRaises(ValueError, raises_error, 'a', b='c')
+
 
 if __name__ == '__main__':
     unittest.main()
