b145382b3be3d036c6032442e42cbad4410ec1ad
Aseem Jain <Aseem.Jain@rackspace.com>
2017-09-06 11:41:21 -0500

    fine refined error messaging for validator

--- json_validator/validator.py
+++ json_validator/validator.py
@@ -6,28 +6,33 @@ import jsonschema
 schema = open("schema.json").read()
 # print (schema)
 
-data = open("data.json").read()
-# data = open("data-error.json").read()
+# data = open("data.json").read()
+data = open("data-error.json").read()
 # print (data)
 
-try:
-    jsonschema.validate(json.loads(data), json.loads(schema))
-except jsonschema.ValidationError as e:
-    print (e.message)
-except jsonschema.SchemaError as e:
-    print (e)
-
+# try:
+#     jsonschema.validate(json.loads(data), json.loads(schema))
+# except jsonschema.ValidationError as e:
+#     print (e.message)
+# except jsonschema.SchemaError as e:
+#     print (e)
+#
 # # Use a Draft3Validator
 # try:
 #     jsonschema.Draft3Validator(json.loads(schema)).validate(json.loads(data))
 # except jsonschema.ValidationError as e:
 #     print (e.message)
-#
-#
-# # Lazily report all errors in the instance
-# try:
-#     v = jsonschema.Draft3Validator(json.loads(schema))
-#     for error in sorted(v.iter_errors(json.loads(data)), key=str):
-#         print(error.message)
-# except jsonschema.ValidationError as e:
-#     print (e.message)
+
+
+# report all errors in the instance
+try:
+    v = jsonschema.Draft3Validator(json.loads(schema))
+    for error in sorted(v.iter_errors(json.loads(data)), key=str):
+        property = str(error.path).replace("deque","",1)
+        if "required" not in error.message:
+            print("value {} for property {}".format(error.message, property))
+        else :
+            print(error.message)
+
+except jsonschema.ValidationError as e:
+    print (e.message)
