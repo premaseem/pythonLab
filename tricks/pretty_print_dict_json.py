942b8af58a4f6247b0f53c4a56d23d89e9d6ebd9
Aseem Jain <premaseem.webmaster@gmail.com>
2018-02-21 10:28:30 -0600

    python refactored

--- tricks/pretty_print_dict_json.py
+++ tricks/pretty_print_dict_json.py
@@ -3,12 +3,12 @@ __author__ = 'asee2278'
 import json
 dict = {"a":1,'c':2,"b":34,"city":"san Antonio"}
 
-print dict
+print(dict)
 
-print json.dumps(dict,indent=2,sort_keys=True)
+print(json.dumps(dict, indent=2, sort_keys=True))
 
 
-# Output
+""" Sample Output """
 # /Users/asee2278/virtualEnv/bpiNew/bpi/bin/python /Users/asee2278/gitRepo/playground/pythonPlayground/tricks/pretty_print_dict_json.py
 # {'a': 1, 'city': 'san Antonio', 'c': 2, 'b': 34}
 # {
