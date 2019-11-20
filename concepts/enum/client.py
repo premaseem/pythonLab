bfbb00647567c1c8f531340fc2781727e2c94c60
Prem Aseem Jain <premaseem.webmaster@gmail.com>
2019-11-20 12:20:51 -0600

    value append to enum

--- concepts/enum/client.py
+++ concepts/enum/client.py
@@ -5,5 +5,9 @@ class Main(object):
     def printenum(self):
         print(myenum.NodeStatus.healthy.value)
         print(myenum.NodeStatus.bootstrap.value)
+        st = "healthy"
+        return "labs-"+myenum.NodeStatus[st].value
+
 
 Main().printenum()
+print(Main().printenum())
