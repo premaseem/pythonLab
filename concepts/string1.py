e2e5f726f5bda6bc00e84f3b8ca882d85361a58b
Prem Aseem Jain <premaseem.webmaster@gmail.com>
2019-12-10 11:15:53 -0600

    python string representation

---
+++ concepts/string1.py
@@ -0,0 +1,18 @@
+
+a = 'aseem\'s phone '
+b = "aseem jain "\
+"is a good boy "
+c= """aseem jain is a good boy"""
+
+price_1 = "1100"
+price_2 = "1200"
+#
+# print(id(price_1))
+# price_2 = "12001"
+#
+# print(id(price_1))
+# print(b)
+print(c[:9])
+
+# Reverse string
+print(c[::-1])
