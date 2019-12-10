d1e4dcc2e0e45f8e316c902bb6e787d0771a16aa
Prem Aseem Jain <premaseem.webmaster@gmail.com>
2019-12-10 11:14:53 -0600

    how to call a python script with arguments from another python script

---
+++ concepts/calling_script/caller.py
@@ -0,0 +1,2 @@
+import os
+os.system("python3 main_file.py -d arg1 -p arg2 -e arg3")
---
+++ concepts/calling_script/main_file.py
@@ -0,0 +1,16 @@
+from optparse import OptionParser
+
+if __name__ == '__main__':
+    # provide options
+    parser = OptionParser()
+    parser.add_option("-d", "--database", dest="database", metavar="DATABASE",
+        help="Database product name (REQUIRED)")
+    parser.add_option("-e", "--environment", dest="environment", metavar="ENVIRONMENT",
+        help="Database product name (REQUIRED)")
+    parser.add_option("-p", "--patchfile", dest="patchfile", metavar="PATCHFILE",
+        help="Patch file path in S3 to execute. Should be TAR file.(REQUIRED)")
+    parser.add_option("-m", "--max_thread", dest="max_thread", metavar="MAX_THREAD",
+        help="Max number of con current processes.(OPTIONAL)")
+
+    (options, args) = parser.parse_args()
+    print(options)
