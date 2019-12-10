83fb1173ca696f8c48bf34a74ecfc753bf16bd3c
Prem Aseem Jain <premaseem.webmaster@gmail.com>
2019-12-10 13:16:56 -0600

    Objective is to add or override value in dict

---
+++ concepts/adding_value_to_dict.py
@@ -0,0 +1,19 @@
+# Objective is to add or override value in dict
+# @Author Aseem Jain
+
+mydict = {"key1": "value",
+"key2": "value",
+"key3": "value"
+         }
+print(mydict)
+
+# add required key over here
+
+mydict['key4'] = "value4"
+mydict['key3'] = "updated value"
+
+print(mydict)
+
+print(mydict.get("key3"))
+print(mydict["key3"])
+
