3d84bd1710d4300b293ccf8b510f0f3b5e0b798f
Prem Aseem Jain <premaseem.webmaster@gmail.com>
2020-01-13 23:37:41 -0600

    python print formatting and different types

---
+++ concepts/print.py
@@ -0,0 +1,37 @@
+# objective is to learn different type of print statements and option
+
+kind = "good"
+weight = 140
+# simple
+print("aseem is a good man")
+
+# multi line print
+print(""" Aseem jain
+is really 
+a good person""")
+
+# new line and tabbed
+print("aseem is \t a good \n man")
+
+# concat vars with +
+print("aseem jain is a "+ kind + " man.")
+
+# interpolate with ,
+print("aseem jain is a ", kind , " man.")
+
+
+# in the same line
+print("aseem is a good man", end= " ")
+print("after end")
+
+# with variable
+print("aseem is a good man weight", weight)
+
+# with format
+print("aseem is a {} man with {} pounds weight".format(kind, weight))
+
+# with printf
+print(f"aseem is a {kind} man with {weight} lbs")
+
+# with variables place holders
+print("aseem is a {k} man with {w} pounds weight".format(w=weight,k=kind))
