35043939d8dd145b5306588fe3e081593101bd5d
Prem Aseem Jain <premaseem.webmaster@gmail.com>
2020-01-22 23:51:04 -0600

    pretty print list

---
+++ tricks/pretty_print_list.py
@@ -0,0 +1,10 @@
+
+names = ["sapna", "ranu", "sandhya", "swati", "trishla"]
+print(names)
+# output
+# ['sapna', 'ranu', 'sandhya', 'swati', 'trishla']
+
+# pretty print with join
+print(", ".join(names))
+# output
+# sapna, ranu, sandhya, swati, trishla
