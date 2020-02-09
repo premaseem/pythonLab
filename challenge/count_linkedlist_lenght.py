e3eee1d9dfb4388ad35d3eb9c10e0e8487f978c0
Prem Aseem Jain <premaseem.webmaster@gmail.com>
2020-02-08 23:00:08 -0600

    # count linked list lenght in iterative way and recursive way

---
+++ challenge/count_linkedlist_lenght.py
@@ -0,0 +1,47 @@
+# count linked list lenght in iterative way and recursive way
+
+class Node:
+    def __init__(self, d, n=None):
+        self.d = d
+        self.n = n
+
+    def __str__(self):
+        return str(self and self.d) + ", " + str(self.n and self.n)
+
+
+class ll:
+    def __init__(self):
+        self.head = None
+
+    def add(self, d):
+        if not self.head:
+            self.head = Node(d)
+            return
+
+        self.head = Node(d, self.head)
+
+    def len_itr(self):
+        cn = self.head
+        count = 0
+        while cn:
+            count += 1
+            cn = cn.n
+        return count
+
+    def len_rec(self, node, cnt=0):
+        if not node:
+            return cnt
+        cnt += 1
+        node = node.n
+        return self.len_rec(node, cnt)
+
+
+o = ll()
+o.add(1)
+o.add(2)
+o.add(3)
+o.add(4)
+
+print(o.head)
+assert 4 == o.len_itr()
+assert 4 == o.len_rec(o.head)
