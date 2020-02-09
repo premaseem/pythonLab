1fa03195e4abd7ceb3d7941a12acfd2597e02354
Prem Aseem Jain <premaseem.webmaster@gmail.com>
2020-02-09 01:09:58 -0600

    swap head node

--- challenge/count_linkedlist_lenght.py
+++ challenge/count_linkedlist_lenght.py
@@ -40,14 +40,58 @@ class ll:
             return 0
         return 1 + self.len_rec_1(node.n )
 
+    def swap(self, s1, s2):
+        c = self.head
+        p = None
+        while c:
+            if c.d == s1:
+                break
+            p = c
+            c = c.n
+        p1 = p
+        c1 = c
+
+        c = self.head
+        p = None
+        while c:
+            if c.d == s2:
+                break
+            p = c
+            c = c.n
+        p2 = p
+        c2 = c
+
+        if not c1 or not c2:
+            return
+
+        if p1:
+            p1.n = c2
+        else:
+            self.head = c2
+
+        if p2:
+            p2.n = c1
+        else:
+            self.head = c1
+
+        c1.n, c2.n = c2.n, c1.n
+
+
+
 
 o = ll()
-o.add(1)
-o.add(2)
-o.add(3)
-o.add(4)
+o.add("A")
+o.add("B")
+o.add("C")
+o.add("D")
+o.add("E")
 
 print(o.head)
-assert 4 == o.len_itr()
-assert 4 == o.len_rec(o.head)
-assert 4 == o.len_rec_1(o.head)
+assert 5 == o.len_itr()
+assert 5 == o.len_rec(o.head)
+assert 5 == o.len_rec_1(o.head)
+
+# swap C and B
+o.swap("A","E")
+print(o.head)
+
