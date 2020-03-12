a65f41442e44feda862779415f190fc70b81e5a0
Prem Aseem Jain <premaseem.webmaster@gmail.com>
2020-03-12 15:43:08 -0500

    MongoDB interview question: given a Binary tree, need to figure out if all node have identical values.

---
+++ interview_question/tree_with_similar_nodes_MONGODB.py
@@ -0,0 +1,16 @@
+# This coding exercise was asked during mongodb interview.
+
+# Question: A function takes input of binary tree, returns True or False
+# Given: a Binary tree, need to figure out if all node have identical values.
+
+
+#         1
+#     1       1
+#   1   1   1   1
+# Returns True
+
+#         1
+#     1       2
+#   1   1   1   1
+# Returns False
+
