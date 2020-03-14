dbd018d1c1851c6c23055044b6c6c548ef765757
Prem Aseem Jain <premaseem.webmaster@gmail.com>
2020-03-14 01:38:51 -0500

    string short cuts

--- CHEAT_SHEET.py
+++ CHEAT_SHEET.py
@@ -9,6 +9,31 @@ if not myString:
 #####################################
 # Remove white space from back and front of string
 
-myString = " aseem    jain   "
-myString.strip()
+myString = "    aseem    jain   "
+a = myString.strip(" ")
+print(a)
+#####################################
+
+# Split: Return a list of the words in the string, using sep as the delimiter string.
+arr = "to-do-is-nice".split("-")
+print(arr)
+#####################################
+
+# Join: The string whose method is called is inserted in between each given string.
+str = "+".join(arr)
+print(str)
+#####################################
+
+# Count: Return the number of non-overlapping occurrences of substring
+print("aseem".count("e"))
+#####################################
+# find char in word, can also specify length range returns -1 if not find, else index
+
+"aseem".find("s")
+print("aseem jain".find("m",0,5))
+#####################################
+
+# Replace: Return a copy with all occurrences of substring old replaced by new.
+print("aseem".replace("e","x"))
+
 
