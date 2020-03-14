0055e79ba440c66654d52496e056fdd5a22120d8
Prem Aseem Jain <premaseem.webmaster@gmail.com>
2020-03-14 00:58:11 -0500

    Cheat sheet with shortcuts

---
+++ CHEAT_SHEET.py
@@ -0,0 +1,14 @@
+# Find out if string is not blank or None
+
+myString = None
+myString = ""
+
+if not myString:
+    print("invalid string")
+
+#####################################
+# Remove white space from back and front of string
+
+myString = " aseem    jain   "
+myString.strip()
+
