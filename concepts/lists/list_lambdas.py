606520f32a197b9896bb34e06f73ac286bde23fd
Prem Aseem Jain <premaseem.webmaster@gmail.com>
2020-09-28 22:47:00 -0500

    lambda

--- concepts/lists/list_lambdas.py
+++ concepts/lists/list_lambdas.py
@@ -9,3 +9,18 @@ for i in range(0,len(L)):
 
     # will invoke lambda functions
     print(L[i](i))
+
+""" 
+adding lambda in list comprehension as one line function. 
+"""
+
+l = [1,2,3,4]
+def convert_to_str(x):
+    return x
+
+# calling external fun on each element
+l = [convert_to_str(x) for x in l]
+
+# calling lambda fun on each element
+l = [(lambda x:str(x*x))(x) for x in l]
+print(l)
