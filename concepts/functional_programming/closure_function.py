d7ee955cdc1f0e51352af86dd7ea891d1ef5c6b8
Prem Aseem Jain <premaseem.webmaster@gmail.com>
2020-10-04 17:54:42 -0500

    closure with class construct to save variable scope

--- concepts/functional_programming/closure_function.py
+++ concepts/functional_programming/closure_function.py
@@ -1,15 +1,28 @@
 """
 The closure contains enclosed scope of function.
-with the init variable
+with the init variable,
+
 """
+class Multiplier:
+    def __init__(self,n):
+        self.n = n
+
+    def multiply(self, m):
+        return self.n * m
+
 
 def make_multiplier_of(n):
-    def multiplier(m):
+    def multiply(m):
 
         return n * m
-    return multiplier
+    return multiply
 
 table5 = make_multiplier_of(5)
 table2 = make_multiplier_of(2)
 print(table5(10))
 print(table2(10))
+
+obj5 = Multiplier(5)
+obj2 = Multiplier(2)
+print(obj5.multiply(10))
+print(obj5.multiply(7))
