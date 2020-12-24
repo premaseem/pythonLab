b045f52e7351ed5d542cc9db2baa507ca0f0e75e
Prem Aseem Jain <premaseem.webmaster@gmail.com>
2020-12-24 15:48:23 -0600

    binary tree with traversal Preorder -  NLR inorder -   LNR postorder - LRN bredth first

--- algorithm_and_datastructure/tree/binary_tree.py
+++ algorithm_and_datastructure/tree/binary_tree.py
@@ -16,6 +16,8 @@ class BinaryTree(object):
             return self.inorder_print(tree.root, "")
         elif traversal_type == "postorder":
             return self.postorder_print(tree.root, "")
+        elif traversal_type == "levelorder":
+            return self.levelorder_print(tree.root)
 
         else:
             print("Traversal type " + str(traversal_type) + " is not supported.")
@@ -45,6 +47,50 @@ class BinaryTree(object):
             traversal += (str(start.value) + "-")
         return traversal
 
+    def levelorder_print(self, start):
+        if start is None:
+            return
+
+        queue = Queue()
+        queue.enqueue(start)
+
+        traversal = ""
+        while len(queue) > 0:
+            traversal += str(queue.peek()) + "-"
+            node = queue.dequeue()
+
+            if node.left:
+                queue.enqueue(node.left)
+            if node.right:
+                queue.enqueue(node.right)
+
+        return traversal
+
+class Queue(object):
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop()
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1].value
+
+    def __len__(self):
+        return self.size()
+
+    def size(self):
+        return len(self.items)
+
+
 # 1-2-4-5-3-6-7-
 # 4-2-5-1-6-3-7
 # 4-2-5-6-3-7-1
@@ -63,6 +109,7 @@ tree.root.left.right = Node(5)
 tree.root.right.left = Node(6)
 tree.root.right.right = Node(7)
 
-#print(tree.print_tree("preorder"))
-#print(tree.print_tree("inorder"))
+print(tree.print_tree("preorder"))
+print(tree.print_tree("inorder"))
 print(tree.print_tree("postorder"))
+print(tree.print_tree("levelorder"))
