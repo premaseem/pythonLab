036ee5b335c936007df333944c00302eb53ac4f8
Prem Aseem Jain <premaseem.webmaster@gmail.com>
2021-01-02 00:41:45 -0600

    merge sorted lists with inplace solution

--- challenge/array/merge_sorted_lists.py
+++ challenge/array/merge_sorted_lists.py
@@ -19,3 +19,19 @@ l1 = [2,4,6,8]
 l2 = [3,5,7,8,9]
 print(merge_lists(l1,l2))
 
+###################
+# In place merge list
+def merge_lists_inplace(l1,l2):
+    li1 = li2 = 0
+    while li1 < len(l1) and li2 < len(l2):
+        if l1[li1] > l2[li2]:
+            l1.insert(li1, l2[li2])
+            li2 += 1
+        else:
+            li1 += 1
+    l1.extend(l2[li2:])
+    return l1
+
+l1 = [2,4,6,8]
+l2 = [3,5,7,8,9]
+print(merge_lists_inplace(l1,l2))
