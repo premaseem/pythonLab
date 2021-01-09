18e4d4f20936789e2d546eb4d5a8173b58cd9add
Prem Aseem Jain <premaseem.webmaster@gmail.com>
2021-01-09 16:34:55 -0600

    hashing implementation for multi values

--- algorithm_and_datastructure/hashing/hashing.py
+++ algorithm_and_datastructure/hashing/hashing.py
@@ -25,6 +25,19 @@ class HashTable:
         if -1 == self.retrieve_data(k.id):
             self.buckets[self.generate_hash(k.id)].append(k)
 
+    def remove_data(self,k):
+        if -1 != self.retrieve_data(k.id):
+            print("removing ",k)
+            bucket = self.buckets[self.generate_hash(k.id)]
+            for i, e in enumerate(bucket):
+                if k.id == e.id:
+                    print("removing inside loop",k)
+                    bucket.pop(i)
+
+    def update_data(self,k):
+        self.remove_data(k)
+        self.buckets[self.generate_hash(k.id)].append(k)
+
     def retrieve_data(self,k):
         bucket = self.buckets[self.generate_hash(k)]
         for e in bucket:
@@ -38,9 +51,14 @@ if __name__ == "__main__":
     hd = HashData("prem","email","phone")
     o.insert_data(hd)
     # o.insert_data("prem")
-    hd = HashData("prem","fax",("bra","penty"))
+    hd = HashData("prem","fax","bra","penty")
     o.insert_data(hd)
+    ash = HashData("seema","ashim")
+    o.insert_data(ash)
+    print(o.buckets)
     # o.insert_data("seema")
+    o.remove_data(ash)
+    o.update_data(hd)
     print(o.buckets)
     print(o.retrieve_data("prem"))
     print(o.retrieve_data("aseem1"))
