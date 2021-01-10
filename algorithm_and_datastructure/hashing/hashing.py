024d833ea3b556a088ba9a3ca31dd0cba5ce548e
Prem Aseem Jain <premaseem.webmaster@gmail.com>
2021-01-09 23:35:05 -0600

    trigger resize when hash table is occpied more then 60%

--- algorithm_and_datastructure/hashing/hashing.py
+++ algorithm_and_datastructure/hashing/hashing.py
@@ -21,9 +21,26 @@ class HashTable:
         hash_index = s % self.size
         return hash_index
 
+    def resize(self):
+        self.size = self.size * 2
+        self.new_bucket = [ [] for e in range(self.size)]
+
+        for i, bucket in enumerate(self.buckets):
+            if bucket:
+                index = self.generate_hash(bucket[0].id)
+                self.new_bucket[index] = bucket
+        print(f"old bucket {self.buckets} new bucket {self.new_bucket}")
+        self.buckets = self.new_bucket
+
+
     def insert_data(self,k):
         if -1 == self.retrieve_data(k.id):
             self.buckets[self.generate_hash(k.id)].append(k)
+        op = sum([1 for x in self.buckets if x])
+        if op > self.size * 0.6:
+            # trigger resize
+            print("triggered resize")
+            self.resize()
 
     def remove_data(self,k):
         if -1 != self.retrieve_data(k.id):
@@ -52,13 +69,27 @@ if __name__ == "__main__":
     o.insert_data(hd)
     # o.insert_data("prem")
     hd = HashData("prem","fax","bra","penty")
-    o.insert_data(hd)
+    o.insert_data(HashData("prem","fax","bra","penty"))
     ash = HashData("seema","ashim")
+    o.insert_data(ash)
+    ash = HashData("seema45","ashim")
     o.insert_data(ash)
     print(o.buckets)
     # o.insert_data("seema")
-    o.remove_data(ash)
+    # o.remove_data(ash)
     o.update_data(hd)
     print(o.buckets)
     print(o.retrieve_data("prem"))
     print(o.retrieve_data("aseem1"))
+    # o.resize()
+    ash = HashData("zebra","ashim")
+    o.insert_data(ash)
+    print(o.buckets)
+    o.insert_data(HashData("prem1","fax","bra","penty"))
+    o.insert_data(HashData("prem2","fax","bra","penty"))
+    o.insert_data(HashData("prem3","fax","bra","penty"))
+    o.insert_data(HashData("prem4","fax","bra","penty"))
+    o.insert_data(HashData("prem5","fax","bra","penty"))
+    o.insert_data(HashData("prem6","fax","bra","penty"))
+    o.insert_data(HashData("prem7","fax","bra","penty"))
+    o.insert_data(HashData("prem8","fax","bra","penty"))
