ff4be5720f1cf18ab1a2e8615a6cafd54cca9330
Prem Aseem Jain <premaseem.webmaster@gmail.com>
2021-01-14 22:31:12 -0600

    Global keyword is a keyword that allows a user to modify a variable outside of the current scope. It is used to create global variables from a non-global scope i.e inside a function. Global keyword is used inside a function only when we want to do assignments or when we want to change a variable. Global is not needed for printing and accessing.

--- concepts/globalVars/my_module.py
+++ concepts/globalVars/my_module.py
@@ -1,7 +1,10 @@
 __author__ = 'asee2278'
-
-
-#!/usr/bin/python
+"""
+Global keyword is a keyword that allows a user to modify a variable outside of the current scope. 
+It is used to create global variables from a non-global scope i.e inside a function. 
+Global keyword is used inside a function only when we want to do assignments or when we want to change a variable. 
+Global is not needed for printing and accessing.
+"""
 
 Money = 2000
 def AddMoney():
@@ -14,13 +17,13 @@ AddMoney()
 print(Money)
 
 
-
+accountBalance=100
 class Bank:
 
     accountBalance=10
     def deposit(self,amount):
         global accountBalance
-        accountBalance = accountBalance + amount
+        accountBalance = accountBalance + Money
         return accountBalance
 
 print(Bank().deposit(20))
