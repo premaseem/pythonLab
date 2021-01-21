cb8f700457e1bf913a2325598b26920982382839
Prem Aseem Jain <premaseem.webmaster@gmail.com>
2021-01-20 19:00:18 -0600

    online shopping

--- system_design/library_management_system.py
+++ system_design/library_management_system.py
@@ -25,6 +25,24 @@ The system should be able to send notifications whenever the reserved books beco
 
 Each book and member card will have a unique barcode. The system will be able to read barcodes from books and members’ library cards.
 
+
+We have three main actors in our system:
+
+Librarian: Mainly responsible for adding and modifying books, book items, and users. The Librarian can also issue, reserve, and return book items.
+Member: All members can search the catalog, as well as check-out, reserve, renew, and return a book.
+System: Mainly responsible for sending notifications for overdue books, canceled reservations, etc.
+
+
+Here are the top use cases of the Library Management System:
+
+Add/Remove/Edit book: To add, remove or modify a book or book item.
+Search catalog: To search books by title, author, subject or publication date.
+Register new account/cancel membership: To add a new member or cancel the membership of an existing member.
+Check-out book: To borrow a book from the library.
+Reserve book: To reserve a book which is not currently available.
+Renew a book: To reborrow an already checked-out book.
+Return a book: To return a book to the library which was issued to a member.
+
 """
 
 
--- system_design/parking_lot.py
+++ system_design/parking_lot.py
@@ -29,6 +29,30 @@ Each parking floor should have a display board showing any free parking spot for
 
 The system should support a per-hour parking fee model. For example, customers have to pay $4 for the first hour, $3.5 for the second and third hours, and $2.5 for all the remaining hours.
 
+
+
+Here are the main Actors in our system:
+
+Admin: Mainly responsible for adding and modifying parking floors, parking spots, entrance, and exit panels, adding/removing parking attendants, etc.
+
+Customer: All customers can get a parking ticket and pay for it.
+
+Parking attendant: Parking attendants can do all the activities on the customer’s behalf, and can take cash for ticket payment.
+
+System: To display messages on different info panels, as well as assigning and removing a vehicle from a parking spot.
+
+
+Here are the top use cases for Parking Lot:
+
+Add/Remove/Edit parking floor: To add, remove or modify a parking floor from the system. Each floor can have its own display board to show free parking spots.
+Add/Remove/Edit parking spot: To add, remove or modify a parking spot on a parking floor.
+Add/Remove a parking attendant: To add or remove a parking attendant from the system.
+Take ticket: To provide customers with a new parking ticket when entering the parking lot.
+Scan ticket: To scan a ticket to find out the total charge.
+Credit card payment: To pay the ticket fee with credit card.
+Cash payment: To pay the parking ticket through cash.
+Add/Modify parking rate: To allow admin to add or modify the hourly parking rate.
+
 """
 
 
