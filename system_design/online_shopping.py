46fca5317d1596e463c387abcc94fd56f7fb2522
Prem Aseem Jain <premaseem.webmaster@gmail.com>
2021-01-20 18:57:15 -0600

    online shopping

---
+++ system_design/online_shopping.py
@@ -0,0 +1,40 @@
+"""
+@Author: Aseem Jain
+@profile: https://www.linkedin.com/in/premaseem/
+
+Requirements and Goals of the System
+We will be designing a system with the following requirements:
+
+Users should be able to add new products to sell.
+Users should be able to search for products by their name or category.
+Users can search and view all the products, but they will have to become a registered member to buy a product.
+Users should be able to add/remove/modify product items in their shopping cart.
+Users can check out and buy items in the shopping cart.
+Users can rate and add a review for a product.
+The user should be able to specify a shipping address where their order will be delivered.
+Users can cancel an order if it has not shipped.
+Users should get notifications whenever there is a change in the order or shipping status.
+Users should be able to pay through credit cards or electronic bank transfer.
+Users should be able to track their shipment to see the current state of their order.
+
+
+We have four main Actors in our system:
+
+Admin: Mainly responsible for account management and adding or modifying new product categories.
+Guest: All guests can search the catalog, add/remove items to the shopping cart, as well as become registered members.
+Member: Members can perform all the activities that guests can, in addition to which, they can place orders and add new products to sell.
+System: Mainly responsible for sending notifications for orders and shipping updates.
+
+Here are the top use cases of the Online Shopping System:
+
+Add/update products; whenever a product is added or modified, we will update the catalog.
+Search for products by their name or category.
+Add/remove product items in the shopping cart.
+Check-out to buy product items in the shopping cart.
+Make a payment to place an order.
+Add a new product category.
+Send notifications to members with shipment updates.
+
+"""
+
+
