6ccefa8d956781d062ccc3d74c3f847ccc466047
Prem Aseem Jain <premaseem.webmaster@gmail.com>
2021-02-07 13:44:13 -0600

    reference sheet with reference code or short cuts of python

--- REFERENCE_SHEET.py
+++ REFERENCE_SHEET.py
@@ -6,17 +6,16 @@
 # Returns the max/min value from args or iterable
 # With a single iterable argument, return its biggest item.
 
-total =125
+total = 125
 current = 15
 my_max = max(total, current)
 my_min = min(total, current)
 
-l = [1,4,7,43,23,89,2]
-s = set([1,4,7,43,23,89,2])
+l = [1, 4, 7, 43, 23, 89, 2]
+s = set([1, 4, 7, 43, 23, 89, 2])
 
-my_max =  max(l)
-my_max =  max(s)
-
+my_max = max(l)
+my_max = max(s)
 
 #####################################
 """ String Operations """
@@ -35,7 +34,7 @@ myString = "    aseem    jain   "
 a = myString.strip(" ")
 
 myString = "0000000this is string example....wow!!!0000000";
-print (str.strip( '0' ))
+print(str.strip('0'))
 
 #####################################
 
@@ -104,8 +103,10 @@ def is_num(num):
     except:
         return False
 
+
 # String template for interpolation
 from string import Template
+
 GET_USER_ENDPOINT = Template("$API_ENDPOINT/v2.0/users/$USER_ID")
 final_string = GET_USER_ENDPOINT.substitute(API_ENDPOINT="localhost", USER_ID=786231)
 
@@ -113,10 +114,10 @@ final_string = GET_USER_ENDPOINT.substitute(API_ENDPOINT="localhost", USER_ID=78
 """ Variable assignments """
 #####################################
 # Swap the variable:
-a,b=10,20
-print(a,b)
+a, b = 10, 20
+print(a, b)
 a, b = b, a
-print(a,b)
+print(a, b)
 
 # Ternary assignment:
 max = a if a > b else b
@@ -124,10 +125,9 @@ max = a if a > b else b
 # swap variable without temp
 a, b = b, a
 
-
 # (get_this_if_false, get_this_if_true)[condition]
 age = 20
-can_vote = ("no","yes")[age > 18]
+can_vote = ("no", "yes")[age > 18]
 print("can vote {} ? {}".format(age, can_vote))
 
 #####################################
@@ -138,10 +138,10 @@ print("aseem", end=" ")
 print("jain")
 
 # print a horizontal line by multiplication
-print("-"*40)
+print("-" * 40)
 
 # print in aligned way with with expandtabs
-print("{}:  \t  {} ".format(a,b).expandtabs(50))
+print("{}:  \t  {} ".format(a, b).expandtabs(50))
 print("do it aligned{}:  \t  {} ".format(a, b).expandtabs(50))
 
 #####################################
@@ -150,17 +150,17 @@ print("do it aligned{}:  \t  {} ".format(a, b).expandtabs(50))
 #####################################
 # Figure out if list is not empty
 
-a=[]
+a = []
 if not a:
-  print("List is empty")
+    print("List is empty")
 else:
-  print("do your stuff")
+    print("do your stuff")
 
 #####################################
 # looping over empty list
 
-lst = [1,2,3,4]
-lst2 = [1,2,3,4,6]
+lst = [1, 2, 3, 4]
+lst2 = [1, 2, 3, 4, 6]
 
 # Old school (direct access instead of iterating)
 for i in range(0, len(lst), 1):
@@ -175,26 +175,25 @@ for index, ele in enumerate(lst, start=1):
     print(index, ele)
 
 # Iterating multiple lists or enumerable objects together, runs to shorted of 2
-for e1,e2 in zip(lst,lst2):
-    print(e1,e2)
+for e1, e2 in zip(lst, lst2):
+    print(e1, e2)
 
 # while and for loop evaluate to false if list is empty or none and does throw error
 itr = iter(lst)
 cnt = 0
 while cnt < len(lst):
     cnt += 1
-    try :
+    try:
         print(next(itr))
     except StopIteration:
         pass
 else:
     print("print this if loop is not broken")
 
-
 #####################################
 # List comprehension : convert num list into string list
 
-arr = [3,4,5,4,3,2]
+arr = [3, 4, 5, 4, 3, 2]
 arrs = [str(x) for x in arr]
 
 # normal
@@ -204,12 +203,11 @@ l = [x for x in range(100)]
 lc_if = [x for x in l if x >= 45]
 
 # if / else
-lc_if_else = [x+1 if x >= 45 else x+5 for x in l]
-
+lc_if_else = [x + 1 if x >= 45 else x + 5 for x in l]
 
 #####################################
 # Removing item from list
-list = [2,3,4,0,1,8]
+list = [2, 3, 4, 0, 1, 8]
 
 # Remove an item by value: remove()
 list.remove(0)
@@ -220,22 +218,20 @@ list.pop(2)
 
 # Remove items by index or slice: del
 del list[2]
-del list [2:4]
+del list[2:4]
 
 # Remove items that meet the condition: List comprehensions
-list = [e for e in list if e % 2 == 0 ]
-
+list = [e for e in list if e % 2 == 0]
 
 # Remove all items: clear()
 l.clear()
-
 
 #####################################
 # combining or extending another list
 # Will add all elements of b in a maintaining the order.
 
-a = [1,2,3,4]
-b = [9,6,5,4]
+a = [1, 2, 3, 4]
+b = [9, 6, 5, 4]
 a.extend(b)
 
 # creating 3rd array by adding 2 arrays
@@ -258,21 +254,26 @@ index = vowels.index('e')
 print('The index of e:', index)
 
 #####################################
+# get a random element from a list
+import random
+given_list = [1,2,3,4,5]
+random.choice(given_list)
+
+#####################################
 
 # Generate list with similar items, create list with 5 false elements
 visited = [False] * 5
 
-
 #####################################
 # Create new sorted list
 
-a=[5,4,3,7,8,45]
+a = [5, 4, 3, 7, 8, 45]
 new_a = sorted(a, reverse=True)
 
 #####################################
 # Compare 2 lists are equal
-a=[5,4,3,7,8,45]
-b=[4,5,3,7,45,8]
+a = [5, 4, 3, 7, 8, 45]
+b = [4, 5, 3, 7, 45, 8]
 sorted(a) == sorted(b)
 
 #####################################
@@ -289,6 +290,7 @@ arr.reverse()
 # in place reverse the arr with sorting.
 arr.sort(reverse=True)
 
+
 #####################################
 
 # sort custom object
@@ -296,16 +298,19 @@ arr.sort(reverse=True)
 # 2. Dynamic way is by providing lambda func with key.
 
 class Custom():
-    def __init__(self,name,age):
+    def __init__(self, name, age):
         self.name = name
         self.age = age
+
     def __repr__(self):
         return self.name + " " + self.age
+
     def __lt__(self, other):
         return self.age < other.age
 
+
 # obj = Custom("aseem", "jain")
-custom_array = [Custom("aseem", "jain"),Custom("wseem", "jain"),Custom("nehal", "zain") ]
+custom_array = [Custom("aseem", "jain"), Custom("wseem", "jain"), Custom("nehal", "zain")]
 # custom_array=[]
 print(custom_array)
 custom_array.sort()
@@ -313,6 +318,18 @@ custom_array.sort(key=lambda x: x.name, reverse=True)
 print(custom_array)
 custom_array.sort(key=lambda x: x.age, reverse=True)
 print(custom_array)
+
+#####################################
+# Type hint in methods
+#####################################
+# func takes 2 args of type int and str and returns str
+
+def sub_this(x: int, obj:Custom) -> str:
+    return f'{obj.name} is {x} year old'
+
+# by using typing, define the element type / generics and get auto completion
+from typing import List
+mylist: List[Custom] = []
 
 #####################################
 """ Dictionaries or Maps"""
@@ -325,8 +342,8 @@ m2 = {}
 m1["k"] = "value"
 
 # get
-v = m1.get("k") # None is return if key does not exists
-v = m1["k"] # key error is thrown if key does not exists
+v = m1.get("k")  # None is return if key does not exists
+v = m1["k"]  # key error is thrown if key does not exists
 
 # delete or remove value
 del m1["k"]
@@ -338,8 +355,8 @@ for k in m1:
 for v in m1.values():
     print(v)
 
-for k,v in m1.items():
-    print(k,v)
+for k, v in m1.items():
+    print(k, v)
 
 # compare maps
 m1 == m2
@@ -359,7 +376,6 @@ for e in input:
     # ['N', 'Y', 'Y', 'N']
     # ['N', 'N', 'N', 'Y']
 
-
 #####################################
 """ SET operation  """
 # set  has no duplicates.
@@ -370,7 +386,7 @@ for e in input:
 
 # create set in 2 ways, argument to set() is an iterable
 set1 = set()
-set2 = {3,6,9,12,15}
+set2 = {3, 6, 9, 12, 15}
 
 # add element
 set1.add(2)
@@ -391,7 +407,7 @@ set1.remove(8)
 set1.discard(8)
 
 # extends or add elements in set, argument is iterable
-set1.update([3,4,], [7,8])
+set1.update([3, 4, ], [7, 8])
 
 # Intersection - common elements
 set1 & set2
@@ -413,13 +429,12 @@ set1.issuperset(set2)
 set1 == set2
 
 # Delta - difference or uncommon elements
-set1 - set2 # elements of set1 - set2
-set2 - set1 # elements of set1 - set2
+set1 - set2  # elements of set1 - set2
+set2 - set1  # elements of set1 - set2
 set1.difference(set2)
 
 # Immutable set or Frozen set, cannot add or updated / ENUM
-froze_set = frozenset([1,2,3,])
-
+froze_set = frozenset([1, 2, 3, ])
 
 #####################################
 """ MAP or DICTIONARY   """
@@ -429,7 +444,7 @@ froze_set = frozenset([1,2,3,])
 #####################################
 
 # creating map
-m = {"a":1}
+m = {"a": 1}
 
 # adding
 m["key"] = "value"
@@ -463,7 +478,7 @@ for k in m.values(): print(k)
 for k in m.items(): print(k)
 
 # iterate and unpack keys and values
-for k,v in m.items(): print(k,v)
+for k, v in m.items(): print(k, v)
 
 # update map , update existing keys and adds new keys
 d1 = {'a': 10, 'b': 20, 'c': 30}
@@ -473,4 +488,66 @@ d1.update(b=200, d=400)
 # Merge dictionary with update
 d1 = {'a': 10, 'b': 20, 'c': 30}
 d2 = {'b': 200, 'd': 400}
+
+
 # d1 => {'a': 10, 'b': 200, 'c': 30, 'd': 400}
+
+
+#####################################
+""" pytest Testing    """
+#####################################
+import pytest
+
+# Run test by keyword or substring in test name
+# $ pytest -k great -v
+
+# Mark test to run
+#@pytest.mark.<markername>
+
+# $ pytest -m <markername> -v
+@pytest.mark.sqr
+def test_sqr():
+    num = 25
+    assert 5 * 5 == num
+# $ pytest -m sqr -v
+
+####################################
+# Data driven testing with all combination of inputs
+@pytest.mark.parametrize("num, output",[(1,11),(2,22),(3,35),(4,44)])
+def test_multiplication_11(num, output):
+    assert 11 * num == output
+
+####################################
+# Setup and Tear down fixtures
+@pytest.fixture()
+def data_service():
+    print("setup objexts")
+    yield [1,2,3,4]
+    print("clean up date")
+
+def test_sum(data_service):
+    assert 10 == sum(data_service)
+
+
+####################################
+# skip a test by annotation
+@pytest.mark.skip
+def test_flaky():
+    pass
+
+####################################
+# run tests with report
+# $ coverage report test_simple.py
+# Name             Stmts   Miss  Cover
+# ------------------------------------
+# test_simple.py      11      1    91%
+# ------------------------------------
+# TOTAL               11      1    91%
+
+####################################
+
+# run test with code coverage in html
+# $ coverage html test_simple.py
+
+# open html converage file
+# open htmlcov/index.html
