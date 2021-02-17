5cae47f908808cac30ac86b9c243c0654c29c12c
Prem Aseem Jain <premaseem.webmaster@gmail.com>
2021-02-17 17:37:29 -0600

    added recursive solution

--- challenge/reverse_string.py
+++ challenge/reverse_string.py
@@ -17,12 +17,50 @@ def sol3(s):
         rev += s[i]
     return rev
 
+def sol4(s):
+    ans = ""
+    l = len(s) -1
+
+    for i, c in enumerate(s):
+        ans += s[l-i]
+    return ans
+
+def sol5(s):
+    s_list = list(s)
+    s_list.reverse()
+    ans = "".join(s_list)
+    return ans
+
+def sol6(s):
+    s_l = list(s)
+    ans = ""
+
+    while s_l:
+        ans += s_l.pop()
+
+    return ans
+
+
+def rec(s):
+    """recursive solution
+    """
+    if len(s) <= 1:
+        return s
+    return rec(s[1:]) + s[0]
+
 
 # tests
 
 assert "meesa" == sol1("aseem")
 assert "meesa" == sol2("aseem")
 assert "meesa" == sol3("aseem")
+assert "meesa" == sol4("aseem")
+assert "meesa" == sol5("aseem")
+assert "meesa" == sol6("aseem")
+assert "meesa" == rec("aseem")
 assert "!evitacudE ot emocleW" == sol1("Welcome to Educative!")
 assert "!evitacudE ot emocleW" == sol2("Welcome to Educative!")
 assert "!evitacudE ot emocleW" == sol3("Welcome to Educative!")
+assert "!evitacudE ot emocleW" == sol4("Welcome to Educative!")
+assert "!evitacudE ot emocleW" == sol5("Welcome to Educative!")
+assert "!evitacudE ot emocleW" == sol6("Welcome to Educative!")
