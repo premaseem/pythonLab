23f6e9488b1f480244c0b1e5be12975207cf8d94
Aseem Jain <Aseem.Jain@rackspace.com>
2021-02-26 00:13:38 -0600

    reverse num

--- challenge/numbers/reverse_signed_numbers.py
+++ challenge/numbers/reverse_signed_numbers.py
@@ -1,16 +1,32 @@
+def sol( x: int) -> int:
+    is_neg = x < 0
+    rev = str(abs(x))[::-1]
+    val = int(rev)
+    if val > 2147483647 or val < -2147483647:
+        return 0
+    if is_neg:
+        return  -1 * val
+    return val
+
+def sol1(n):
+    m = -1 if n < 0 else 1
+    n = abs(n)
+    ans = 0
+    while n > 0:
+        ans *= 10
+        r = n % 10
+        n = n // 10
+        ans += r
+    return m * ans
 
 
-def rev(n):
-    s=""
-    if n < 0:
-       s="-"
-       n=str(n)[1:]
-
-    r = str(n)[-1::-1]
-    while r[0] == "0":
-        r=r[1:]
-    return int(s+r)
-
-assert 123 == rev(321)
-assert -123 == rev(-321)
-assert -21 == rev(-120)
+test_data = [
+    (123 ,321),
+    (-1234 ,-4321),
+    (0 ,0),
+    (100 ,1),
+]
+for given, expected in test_data:
+    assert expected == sol(given)
+    assert expected == sol1(given)
+    print(f"Test passed for: given {given} and expected = {expected}")
